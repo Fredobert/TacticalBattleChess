ade838ff84ac4b7953fc68b8ca2891ac3db9f3cf
FRED <grunack@mail.hs-ulm.de>
2018-05-21 21:41:56 +0200

    added existing project

---
+++ fredtestrepos/FredFirst/Assets/scripts/fires.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fires : MonoBehaviour {
+    public float dtime = 2f;
+    public float speed = 10f;
+    public float dx = 0f;
+    public float dy = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * dx * Time.deltaTime, 0f, speed * dy * Time.deltaTime);
+        dtime -= Time.deltaTime;
+        if (dtime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
